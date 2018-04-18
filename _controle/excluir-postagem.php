534145b663f37e83182c1dac5c2def567833d8c6
rennkatt <33918121+rennkatt@users.noreply.github.com>
2018-04-17 21:54:31 -0300

    atualizando administrativo

---
+++ _controle/excluir-postagem.php
@@ -0,0 +1,56 @@
+<?php
+
+	 //excluir
+	if(isset($_GET['delete'])){
+		$id_delete = $_GET['delete'];
+		
+		
+		
+		// seleciona a imagem
+		$seleciona = "SELECT * from tb_postagens WHERE id= :id_delete";
+		try{
+			$result = $conexao->prepare($seleciona);	
+			$result->bindParam('id_delete',$id_delete, PDO::PARAM_INT);		
+			$result->execute();
+			$contar = $result->rowCount();
+			if($contar>0){
+				$loop = $result->fetchAll();
+				foreach ($loop as $exibir){
+				}
+				
+				
+				/*
+				$fotoDeleta = $exibir['imagem'];
+				$arquivo = "../_upload/postagens/" .$fotoDeleta;
+				unlink($arquivo);
+				*/
+				
+				// exclui o registo
+				$seleciona = "DELETE from tb_postagens WHERE id=:id_delete";
+				try{
+					$result = $conexao->prepare($seleciona);
+					$result->bindParam('id_delete',$id_delete, PDO::PARAM_INT);				
+					$result->execute();
+					$contar = $result->rowCount();
+					if($contar>0){
+						echo '<div class="alert alert-success">
+                      <button type="button" class="close" data-dismiss="alert">×</button>
+                      <strong>Sucesso!</strong> O post foi excluído.
+                </div>';
+					}else{
+						echo '<div class="alert alert-danger">
+                      <button type="button" class="close" data-dismiss="alert">×</button>
+                      <strong>Erro!</strong> Não foi possível excluir o post.
+                </div>';	
+					}
+					
+					
+					}catch (PDOWException $erro){ echo $erro;}
+			}
+			
+		}catch (PDOWException $erro){ echo $erro;}
+	}
+			
+	
+
+?> 
