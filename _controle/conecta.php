596e0d442303330d510bdd6cfc84d3752e88ba1e
Robson <robsonlemos2011@gmail.com>
2018-04-23 22:48:15 -0300

    atualizando
    atualizando

--- _controle/conecta.php
+++ _controle/conecta.php
@@ -4,5 +4,4 @@
 		//$conexao = new PDO('mysql:host=localhost;dbname=copatur', 'root', '');
 		$conexao ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}catch(PDOException $e){
-	}
-?>
+			echo 'ERROR: ' . $e->getMessage();
