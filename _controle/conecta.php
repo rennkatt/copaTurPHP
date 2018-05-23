c31a1fa233d655589587edff17103d88a4b1d614
root <HyagoDiaz>
2018-05-23 00:09:06 -0300

    Tela final

--- _controle/conecta.php
+++ _controle/conecta.php
@@ -1,5 +1,5 @@
 <?php
-		//$conexao = new PDO('mysql:host=mysql472.umbler.com;dbname=copatur', 'copatur', 'katia123');
+		$conexao = new PDO('mysql:host=mysql472.umbler.com;dbname=copatur', 'copatur', 'katia123');
 		$conexao = new PDO('mysql:host=localhost;dbname=copatur', 'root', '');
 		// $conexao = new PDO('mysql:host=mysql472.umbler.com;dbname=copatur', 'copatur', 'katia123');
 		// $conexao = new PDO('mysql:host=localhost;dbname=copatur', 'root', '');	
