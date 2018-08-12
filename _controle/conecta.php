33d71c5389d9a1350064f2b17cd26f734ec9551a
rennkatt <33918121+rennkatt@users.noreply.github.com>
2018-08-12 16:43:31 -0300

    conecta

--- _controle/conecta.php
+++ _controle/conecta.php
@@ -1,7 +1,5 @@
 <?php
-		$conexao = new PDO('mysql:host=mysql472.umbler.com;dbname=copatur', 'copatur', 'katia123');
-		// $conexao = new PDO('mysql:host=localhost;dbname=copatur', 'root', '');
-		// $conexao = new PDO('mysql:host=mysql472.umbler.com;dbname=copatur', 'copatur', 'katia123');
-		// $conexao = new PDO('mysql:host=localhost;dbname=copatur', 'root', '');	
+		$conexao = new PDO('mysql:host=mysql942.umbler.com;dbname=copatur.com', 'copatur.com', 'katia123');
+		//$conexao = new PDO('mysql:host=localhost;dbname=copatur', 'root', '');
 		$conexao ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
